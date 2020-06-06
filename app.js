44de575ee9af4f586ba1ddcf8266c247dd5fc979
Varlen Neto <varlenneto@poli.ufrj.br>
2020-06-06 18:41:53 -0300

    Rooms creation using socket.io

---
+++ app.js
@@ -0,0 +1,81 @@
+express = require('express');
+app = express();
+
+
+crypto = require('crypto');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+rooms = {};
+
+// Endpoints setup
+app.use(express.static('assets'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/pickups.html');
+});
+
+// Socket.io callbacks
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	console.log(socket.handshake);
+
+    socket.on('room_create', function () {
+
+        let roomCode = crypto.randomBytes(4).toString('hex');
+
+        rooms[roomCode] = {
+            creator : socket.handshake.usercode
+        };
+
+        socket.join(roomCode);
+
+        console.log( socket.id + " created room " + roomCode );
+
+        io.sockets.connected[socket.id].emit('room_entered', { room: roomCode, creator: true });
+
+    });
+
+    socket.on('room_enter', function (roomCode) {
+
+        if (roomCode in rooms) {
+
+            socket.join(roomCode);
+
+            io.sockets.connected[socket.id].emit('room_entered', { room: roomCode, creator: false });
+
+        } else {
+
+            io.sockets.connected[socket.id].emit('room_not_found');
+
+        }
+
+    });
+
+	socket.on('disconnect', function(){
+
+        console.log('a user disconnected');
+        
+        console.log(socket.rooms);
+        
+        // forEach( room => {
+
+        //     if (room.id in rooms) {
+                
+        //         if (io.sockets.adapter.rooms[room.id].length == 0) {
+        //             // Remove local data of room if last user in room disconnected
+        //             delete rooms[room.id];
+        //         }
+
+        //     }
+        // });
+
+	});
+
+});
+
+http.listen(3000, function(){
+
+    console.log('listening on *:3000');
+    
+});
---
+++ assets/pickups.js
@@ -0,0 +1,90 @@
+const API_KEY = "9VOjI3WZVEclNIrSl6pLNpG592wAVH6G";
+let TAG = "infinite";
+const RATING = "R";
+const PERIOD_IN_MILLISECONDS = 5000;
+let URL = "https://api.giphy.com/v1/gifs/random?api_key=" + API_KEY + "&tag=" + TAG + "&rating=" + RATING;
+
+let btnCreateRoom = document.getElementById("btn-create-room");
+let btnJoinRoom = document.getElementById("btn-join-room");
+let txtRoomCode = document.getElementById("txt-room-code");
+let lblStatus = document.getElementById("lbl-status");
+
+const room = {
+  connected : false,
+  code : "",
+  creator: false
+};
+
+function updateBackground(gifUrl) {
+  var backgroundImage = "url(\"" + gifUrl + "\")";
+  $("div.bg").css("background-image", backgroundImage);
+}
+
+function myPeriodicMethod() {
+  $.ajax({
+    url: URL,
+    success: function(data) {
+      console.log(data.data.images.downsized_large.url);
+      updateBackground(data.data.images.downsized_large.url);
+    },
+    complete: function() {
+      // schedule the next request *only* when the current one is complete:
+      setTimeout(myPeriodicMethod, PERIOD_IN_MILLISECONDS);
+    }
+  });
+}
+
+function updateTag() {
+  TAG = $('.tag-input-container input')[0].value;
+  URL = "https://api.giphy.com/v1/gifs/random?api_key=" + API_KEY + "&tag=" + TAG + "&rating=" + RATING;
+}
+
+// schedule the first invocation:
+setTimeout(myPeriodicMethod, 1000);
+
+/*
+ Rooms
+*/
+
+function createRoom() {
+  if (!room.connected)
+    socket.emit('room_create');
+}
+
+function joinRoom() {
+  if (!room.connected)
+    socket.emit('room_enter', txtRoomCode.value);
+}
+
+socket.on('connect', function (){
+  
+  btnCreateRoom = document.getElementById("btn-create-room");
+  btnJoinRoom = document.getElementById("btn-join-room");
+  txtRoomCode = document.getElementById("txt-room-code");
+  lblStatus = document.getElementById("lbl-status");
+
+  btnCreateRoom.removeAttribute("disabled");
+  btnJoinRoom.removeAttribute("disabled");
+  txtRoomCode.removeAttribute("disabled");
+
+  btnCreateRoom.addEventListener("click", function (evt) {
+    createRoom();
+  });
+  
+  btnJoinRoom.addEventListener("click", function (evt) {
+    joinRoom();
+  })
+
+});
+
+socket.on('room_entered', function (data) {
+  btnCreateRoom.setAttribute("disabled", "true");
+  btnJoinRoom.setAttribute("disabled", "true");
+  txtRoomCode.value = data.room;
+  room.code = data.room;
+  room.creator = data.creator;
+  room.connected = true;
+});
+
+
+
